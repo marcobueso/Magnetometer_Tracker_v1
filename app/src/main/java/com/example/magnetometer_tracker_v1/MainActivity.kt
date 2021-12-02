8176cb295087eb58260da1cb8c783d20f96945ea
marcobueso <mbueso97@gmail.com>
2021-12-02 17:19:07 -0600

    Gets Magnetic Readings!! - checkpoint

--- app/src/main/java/com/example/magnetometer_tracker_v1/MainActivity.kt
+++ app/src/main/java/com/example/magnetometer_tracker_v1/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.magnetometer_tracker_v1
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorDisplay: TextView
+
+    // Implemented from SensorEventListener interface
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            val values = event.values
+            for (item in values){
+                println("ITEM: $item")
+            }
+
+            sensorDisplay.text = "reading at 0,1,2: ${values[0].toInt()}, ${values[1].toInt()}, ${values[2].toInt()}"
+
+        }
+    }
+
+    // Implemented from SensorEventListener interface
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // To avoid
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        sensorDisplay = findViewById(R.id.tv_sensorDisplay)
+
+        setUpSensors()
     }
+
+    private fun setUpSensors() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also{
+            // register listener
+            sensorManager.registerListener(this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST) // TODO check different delays
+        }
+    }
+
 }
